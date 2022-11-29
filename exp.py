699636aaa17784b30a3db5769d0f37ae6c66fc04
Michael Brunetti <michael.brunetti@spectralogic.com>
2022-11-29 08:21:09 -0700

    Clean up

--- exp.py
+++
@@ -1,9 +0,0 @@
-
-
-
-def main():
-    charString = "ABC"
-    charexp = "123" + charString[1:]
-    print(charexp)
-if __name__ == "__main__":
-    main()
--- iceHelperFuncs.py
+++ iceHelperFuncs.py
@@ -1,5 +1,6 @@
 import random
 import time
+import paramiko
 
 # Returns the address of a full slot, containerType = "DRIVE" or "SLOT"
 def fullSlot(inventory, containerType):
@@ -18,7 +19,7 @@ def emptySlot(inventory, containerType):
     else:
         return random.choice(emptySlots)["address"]
 
-# Gets the mediaBarcode at a specific address if it exists
+# Gets the mediaBarcode at a specific address if it exists. Used in function inventoryComparison
 def mediaBarcodeInfo(inventory, storageAddress):
     for storageElement in inventory["value"]:
         if storageElement["address"] == storageAddress:
@@ -55,3 +56,85 @@ def poller(requestFunc):
         else:
             print("Polling completed")
             break
+
+# Uses the function getCurrentPackage() request in {libraryType}API.py module to get firmware versions per application.
+# This converts applications into their associated component type and creates a tuple of component type and version.
+# This is used with function firmwareCheck which compares FRU reported FWs to Application FWs.
+def convertedFirmwareVersions(firmwareList):
+    componentList = []
+    for component in firmwareList["firmware"]:
+        if component["name"] == "dip-e":
+            componentList.append(('DRIVE', component['version']))
+        if component["name"] == "motion":
+            componentList.append(('ROBOT', component['version']))
+        if component["name"] == "esw":
+            componentList.append(('NETWORK_SWITCH', component['version']))
+        if component["name"] == "pmm":
+            componentList.append(('PMM', component['version']))
+            componentList.append(('POWER_SUPPLY', component['version']))
+        if component["name"] == "lumos":
+            componentList.append(('LS', component['version']))
+    return componentList
+
+def firmwareFruList(fruList):
+    return [(fru["type"], formatter(fru["fruFirmware"])) for fru in fruList["value"]]
+
+def formatter(firmwareVersion):
+    charString = ""
+    charGroup = ""
+    count = 1
+    for char in firmwareVersion:
+        # a group consists of chars ending with ".", unless it is the last group in the firmware version
+        if char == ".":
+            charGroup += char
+            # This is a group of chars that leads with a 0, exclude leading 0 from firmware string
+            if len(charGroup) > 2 and charGroup[0] == "0":
+                charString += charGroup[1:]
+            # No leading 0 OR 0 is the only character in the group
+            else:
+                charString += charGroup
+            charGroup = ""
+            count += 1
+        # This is the last character in the firmware string
+        elif count == len(firmwareVersion):
+            charGroup += char
+            # This is a group of chars that leads with a 0, exclude leading 0 from firmware string
+            if len(charGroup) >= 2 and charGroup[0] == "0":
+                charString += charGroup[1:]
+            # No leading 0 OR 0 is the only character in the group
+            else:
+                charString += charGroup
+        # Keep adding chars to a group unless one of the above conditions is met
+        else:
+            charGroup += char
+            count += 1
+    return charString
+
+def firmwareCheck(fruFirmware, actualFirmware):
+    mismatchedFirmware = []
+    for fruFw in fruFirmware:
+        match = False
+        for actualFw in actualFirmware:
+            if fruFw == actualFw:
+                match = True
+                break
+        if match is False:
+            mismatchedFirmware.append(fruFw)
+    if not mismatchedFirmware:
+        return True
+    else:
+        return mismatchedFirmware
+
+def systemCtlStatus(host, username, password, application):
+    host = "10.10.11.156"
+    username = "root"
+    password = ""
+    command = f"systemctl status {application}"
+    client = paramiko.client.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(host, username=username, password=password)
+    stdin, stdout, stderr = client.exec_command(command)
+    response = str(stdout.read())
+    client.close()
+    if "active (running)" in response:
+        return True
